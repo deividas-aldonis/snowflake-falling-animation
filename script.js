cd8603d02106dc93b2545c55d6a3c14be2745d8d
Deividas <deividas.aldonis.dev@gmail.com>
2022-12-12 01:01:25 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const snowFlake = `<svg viewbox="0 0 100 100">
+const snowflake = `<svg viewbox="0 0 100 100">
                           <g>
                               <path d="M50 90 v-80" stroke="white" stroke-width="8"></path>
                               <path d="M50 30 L37 18" stroke="white" stroke-width="8"></path>
@@ -27,23 +27,29 @@ const snowFlake = `<svg viewbox="0 0 100 100">
                           </g>
                       </svg>`;
 
+const body = document.body;
+
+body.style.overflow = "hidden";
+body.style.height = "100vh";
+body.style.backgroundImage = "linear-gradient(to top, #a3bded 0%, #6991c7 100%";
+
 function createSnowFlake() {
-  const snowFlakeContainer = document.createElement("div");
-  snowFlakeContainer.innerHTML = snowFlake;
+  const snowflakeContainer = document.createElement("div");
+  snowflakeContainer.innerHTML = snowflake;
 
   const snowFlakeSize = Math.floor(Math.random() * 40) + 20;
-  const startingPoint = Math.floor(Math.random() * 2000) + 60;
+  const startingPoint = Math.floor(Math.random() * 2000) + 200;
 
-  snowFlakeContainer.style.position = "absolute";
-  snowFlakeContainer.style.top = `-${startingPoint}px`;
-  snowFlakeContainer.style.height = `${snowFlakeSize}px`;
-  snowFlakeContainer.style.width = `${snowFlakeSize}px`;
+  snowflakeContainer.style.position = "absolute";
+  snowflakeContainer.style.top = `-${startingPoint}px`;
+  snowflakeContainer.style.height = `${snowFlakeSize}px`;
+  snowflakeContainer.style.width = `${snowFlakeSize}px`;
 
   const xPosition = Math.random() * window.innerWidth;
   const fallDuration = (Math.floor(Math.random() * 10) + 5) * 1000;
   const rotation = Math.floor(Math.random() * 330) + 30;
 
-  snowFlakeContainer.animate(
+  snowflakeContainer.animate(
     [
       { transform: `translateY(0px) translateX(${xPosition}px)` },
       {
@@ -56,13 +62,9 @@ function createSnowFlake() {
     }
   );
 
-  document.body.appendChild(snowFlakeContainer);
+  body.appendChild(snowflakeContainer);
 }
 
-function startAnimation() {
-  for (let i = 0; i < 50; i++) {
-    createSnowFlake();
-  }
+for (let i = 0; i < 50; i++) {
+  createSnowFlake();
 }
-
-startAnimation();
